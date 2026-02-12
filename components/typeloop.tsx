e679dab71b48629e87e818ec3373e4e28525ca01
Vitor Vidotto <vitor.v.m.tozi@gmail.com>
2026-02-12 16:09:31 -0300

    old variables removed

--- components/typeloop.tsx
+++ components/typeloop.tsx
@@ -2,27 +2,27 @@ import Typewriter from 'typewriter-effect';
 
 const TypewriterLoop = () => {
   return (
-    <Typewriter
-      options={{
-        autoStart: true,
-        loop: true,
-        pauseFor: 2500,
-      }}
-      onInit={(typewriter) => {
-        typewriter
-         .typeString('<span style="color: #ffffff;">Eu sou </span><span style="color: #f13232;">Programador</span>')
-          .pauseFor(1500)
-          .deleteChars(11) // Deleta apenas a parte "Programador"
-          .typeString('<span style="color: #33c1ff;">Desenvolvedor</span>')
-          .pauseFor(1500)
-          .deleteChars(14) // Deleta apenas a parte "Desenvolvedor"
-          .typeString('<span style="color: #75ff33;">DevOps</span>')
-          .pauseFor(1500)
-          .deleteChars(6) // Deleta apenas a parte "DevOps"
-          .start();
-      }}
-      style={{ display: 'inline' }}
-    />
+    <span style={{ display: 'inline' }}>
+      <Typewriter
+        options={{
+          autoStart: true,
+          loop: true,
+        }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString('<span style="color: #ffffff;">Eu sou </span><span style="color: #f13232;">Programador</span>')
+            .pauseFor(1500)
+            .deleteChars(11) // Deleta apenas a parte "Programador"
+            .typeString('<span style="color: #33c1ff;">Desenvolvedor</span>')
+            .pauseFor(1500)
+            .deleteChars(14) // Deleta apenas a parte "Desenvolvedor"
+            .typeString('<span style="color: #75ff33;">DevOps</span>')
+            .pauseFor(1500)
+            .deleteChars(6) // Deleta apenas a parte "DevOps"
+            .start();
+        }}
+      />
+    </span>
   );
 };
 
